023b739c5a9a323b3b86c2f006bce05d70387fff
al.zach <al.zacharakis@gmail.com>
2026-06-15 23:52:00 +0300

    feat(ui): add Button shared component

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -2,3 +2,17 @@ export interface LayoutProps {
     children: React.ReactNode;
     addClasses?: string;
 }
+
+export type ButtonProps = {
+    onClick?: () => void;
+    disabled?: boolean;
+    label: string;
+    addClasses?: string;
+}
+
+export type IconButtonProps = {
+    onClick?: () => void;
+    disabled?: boolean;
+    icon: React.ReactNode;
+    addClasses?: string;
+}
