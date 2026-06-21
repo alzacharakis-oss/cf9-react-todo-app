ff3acbb10e4e99498af5a7ca56c5d57c23008e3d
al.zach <al.zacharakis@gmail.com>
2026-06-21 19:53:51 +0300

    feat(todo) : finalize TodoForm component

--- src/features/todo/TodoForm.tsx
+++ src/features/todo/TodoForm.tsx
@@ -1,20 +1,37 @@
 import Button from "../../shared/ui/Button.tsx";
 import type {TodoFormProps} from "./types.ts";
+import {useState} from "react";
 
-const TodoForm = ({inputRef}: TodoFormProps) => {
-    return (
-        <>
-            <form className="flex gap-4 mb-4">
-                <input
-                    type="text"
-                    ref={inputRef}
-                    className="flex-1 border p-2 rounded"
-                    placeholder="New task..."
-                />
-                <Button label="Add"/>
-            </form>
-        </>
-    )
-}
+const TodoForm = ({onAdd, inputRef}: TodoFormProps) => {
+    const [text, setText] = useState("");
 
-export default TodoForm;
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setText(e.target.value);
+    }
+
+    const handleSubmit = (e: React.SubmitEvent) => {
+        e.preventDefault();
+        if (text.trim() !== ""){
+            onAdd(text);
+            setText("");
+        }
+    }
+
+        return (
+            <>
+                <form className="flex gap-4 mb-4" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        ref={inputRef}
+                        value={text}
+                        onChange={handleChange}
+                        className="flex-1 border p-2 rounded"
+                        placeholder="New task..."
+                    />
+                    <Button label="Add"/>
+                </form>
+            </>
+        )
+    }
+
+    export default TodoForm;
